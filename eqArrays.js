8451574cd6b58f3df59bce852abfd878ceff3c6d
Andres <andres039039@Hotmail.com>
2021-10-27 00:14:02 +0000

    eqArray function created

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+"use strict";
+const assertEqual = function (actual, expected) {
+  const log = console.log;
+  if (actual === expected) {
+    log(`🥳Assertion Passed: 🏁${actual} === ${expected}`);
+  } else {
+    log(`😞Assertion Failed: 🎌${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    return arr1.reduce((result, element, index) => {
+      if (result === false) {cv
+        cv
+          result = true;
+        } else {
+          result = false;
+          return result;
+        }
+      }
+      return result;
+    }, true);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 0, 4, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(
+  eqArrays(
+    ["homer", "simpson", "is", "great"],
+    ["homer", "simpson", "is", "great"]
+  ),
+  true
+);
+assertEqual(eqArrays([true, true, false], [true, true, false]), true);
