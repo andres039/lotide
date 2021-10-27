1710a2f879fb24b51ae5534c5920683add9bbede
Andres <andres039039@Hotmail.com>
2021-10-27 21:30:00 +0000

    countLetters function

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+"use strict";
+const assertEqual = function (actual, expected) {
+  const log = console.log;
+  if (actual === expected) {
+    log(`🥳Assertion Passed: 🏁${actual} === ${expected}`);
+  } else {
+    log(`😞Assertion Failed: 🎌${actual} !== ${expected}`);
+  }
+};
+const countLetters = function (str) {
+  /*
+  input a string
+  make the string into an array
+  generate an object with letters as keys and a number as value
+  output an object
+  */
+  const arrOfLetters = str.split("");
+  return arrOfLetters.reduce((lettersCount, letter) => {
+    letter in lettersCount
+      ? lettersCount[letter]++
+      : (lettersCount[letter] = 1);
+    return lettersCount;
+  }, {});
+};
