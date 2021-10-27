1487a9fe5245c5a44f0ef7885b7cd97776d331b6
Andres <andres039039@Hotmail.com>
2021-10-27 21:49:27 +0000

    letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+"use strict";
+const assertEqual = function (actual, expected) {
+  const log = console.log;
+  if (actual === expected) {
+    log(`🥳Assertion Passed: 🏁${actual} === ${expected}`);
+  } else {
+    log(`😞Assertion Failed: 🎌${actual} !== ${expected}`);
+  }
+};
+const letterPositions = function (sentence) {
+  const arrayOfLetters = sentence.split("");
+  return arrayOfLetters.reduce((lettersByIndex, letter, index) => {
+    letter !== " "
+      ? !lettersByIndex[letter]
+        ? (lettersByIndex[letter] = [index])
+        : lettersByIndex[letter].push(index)
+      : "do nothin";
+    return lettersByIndex;
+  }, {});
+};
+
