b871c348e249a8097e9e4c9570b41111afcfb40b
Andres <andres039039@Hotmail.com>
2021-10-27 00:30:37 +0000

    defined function assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    return arr1.reduce((result, element, index) => {
+      if (result === false) {
+        return result;
+      } else {
+        if (arr2[index] === element) {
+          result = true;
+        } else {
+          result = false;
+          return result;
+        }
+      }
+      return result;
+    }, true);
+  }
+};
+const assertArraysEqual = function (arr1, arr2) {
+  const areEqual = eqArrays(arr1, arr2);
+  const log = console.log;
+  if (areEqual) {
+    log(`🥳Assertion Passed: 🏁${arr1} === ${arr2}`);
+  } else {
+    log(`😞Assertion Failed: 🎌${arr1} !== ${arr2}`);
+  }
+};
