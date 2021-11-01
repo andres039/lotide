7959f60917fbfd50cb55669d276a8733380bffc6
Andres <andres039039@Hotmail.com>
2021-11-01 17:06:20 +0000

    tail module added

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
 "use strict";
-const assertEqual = function (actual, expected) {
-  const log = console.log;
-  if (actual === expected) {
-    log(`🥳Assertion Passed: 🏁${actual} === ${expected}`);
-  } else {
-    log(`😞Assertion Failed: 🎌${actual} !== ${expected}`);
-  }
-};
 const tail = (arr) => arr.filter((element, index) => index > 0);
+module.exports = tail
