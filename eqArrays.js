4dc46179d87aed8cbfa53c7c2de868e5f1c330bd
Andres <andres039039@Hotmail.com>
2021-11-01 17:15:15 +0000

    eqArrays module

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
 "use strict";
-const assertEqual = function (actual, expected) {
-  const log = console.log;
-  if (actual === expected) {
-    log(`🥳Assertion Passed: 🏁${actual} === ${expected}`);
-  } else {
-    log(`😞Assertion Failed: 🎌${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -27,14 +19,4 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 0, 4, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(
-  eqArrays(
-    ["homer", "simpson", "is", "great"],
-    ["homer", "simpson", "is", "great"]
-  ),
-  true
-);
-assertEqual(eqArrays([true, true, false], [true, true, false]), true);
+module.exports = eqArrays
